39d464665a6d04cc765272222eb87597e0d4b4e7
dizcza <dizcza@gmail.com>
2020-08-03 21:23:52 +0200

    USE_FULL_ASSERT

--- Src/main.c
+++ Src/main.c
@@ -198,6 +198,10 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	LCD_SetMode(LCD_MODE_DRAW);
+	LCD_SetTextColor(RED, BLACK);
+	LCD_Printf("assert_failed: %s, line %lu\n", file, line);
+	while (1);
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
